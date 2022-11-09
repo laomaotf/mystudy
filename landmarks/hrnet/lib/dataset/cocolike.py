dc1eb5af996303586638522c7fed8efe3dae36da
laomaotf <laomaotf@outlook.com>
2022-11-09 21:23:41 +0800

    add landmark/hrnet

---
+++ landmarks/hrnet/lib/dataset/cocolike.py
@@ -0,0 +1,457 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+import os,cv2
+import torch
+import json_tricks as json
+from collections import OrderedDict
+import copy
+import numpy as np
+
+import random
+from torch.utils.data import Dataset
+
+from collections import defaultdict
+from lib.nms.nms import oks_nms
+from lib.nms.nms import soft_oks_nms
+
+
+from lib.utils.transforms import get_affine_transform
+from lib.utils.transforms import affine_transform
+from lib.utils.transforms import fliplr_joints
+
+
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+logger = logging.getLogger(__name__)
+
+
+class COCOLikeDataset(Dataset):
+    def __init__(self, cfg, root, image_set, is_train, transform=None):
+        self.num_joints = 0
+
+        self.is_train = is_train
+        self.root = root
+        self.image_set = image_set
+
+        self.output_path = cfg.OUTPUT_DIR
+        self.data_format = cfg.DATASET.DATA_FORMAT
+
+        self.scale_factor = cfg.DATASET.SCALE_FACTOR
+        self.rotation_factor = cfg.DATASET.ROT_FACTOR
+        self.color_rgb = cfg.DATASET.COLOR_RGB
+
+        self.image_size = np.array(cfg.MODEL.IMAGE_SIZE)
+        self.heatmap_size = np.array(cfg.MODEL.HEATMAP_SIZE)
+        self.sigma = cfg.MODEL.SIGMA
+
+        self.transform = transform
+        self.db = []
+ 
+        
+        
+        self.flip_pairs = cfg.DATASET.FLIP_PAIRS
+        
+        #self.pixel_std = 200.0 #see get_affine_transform() for detail of 200
+        image_width = cfg.MODEL.IMAGE_SIZE[0]
+        image_height = cfg.MODEL.IMAGE_SIZE[1]
+        self.aspect_ratio = image_width * 1.0 / image_height
+
+        self.db = self._get_db()
+        
+        
+        self.sigmas = np.array([0.03 for _ in range(self.num_joints)])
+   
+
+        if is_train and cfg.DATASET.SELECT_DATA:
+            self.db = self.select_data(self.db)
+
+        logger.info('=> load {} samples'.format(len(self.db)))
+
+    def __len__(self,):
+        return len(self.db)
+
+    def __getitem__(self, idx):
+        db_rec = copy.deepcopy(self.db[idx])
+
+        image_file = db_rec['image']
+        filename = db_rec['filename'] if 'filename' in db_rec else ''
+        imgnum = db_rec['imgnum'] if 'imgnum' in db_rec else ''
+
+        if self.data_format == 'zip':
+            from utils import zipreader
+            data_numpy = zipreader.imread(
+                image_file, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION
+            )
+        else:
+            data_numpy = cv2.imread(
+                image_file, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION
+            )
+
+        if self.color_rgb:
+            data_numpy = cv2.cvtColor(data_numpy, cv2.COLOR_BGR2RGB)
+
+        if data_numpy is None:
+            logger.error('=> fail to read {}'.format(image_file))
+            raise ValueError('Fail to read {}'.format(image_file))
+
+        joints = db_rec['joints_3d']
+        joints_vis = db_rec['joints_3d_vis']
+
+        c = db_rec['center']
+        s = db_rec['scale']
+        score = db_rec['score'] if 'score' in db_rec else 1
+        r = 0
+
+        if self.is_train:
+            sf = self.scale_factor
+            rf = self.rotation_factor
+            s = s * np.clip(np.random.randn()*sf + 1, 1 - sf, 1 + sf)
+            r = np.clip(np.random.randn()*rf, -rf*2, rf*2) \
+                if random.random() <= 0.6 else 0
+
+            if self.flip_pairs != [] and random.random() <= 0.5:
+                data_numpy = data_numpy[:, ::-1, :]
+                joints, joints_vis = fliplr_joints(
+                    joints, joints_vis, data_numpy.shape[1], self.flip_pairs)
+                c[0] = data_numpy.shape[1] - c[0] - 1
+
+        trans = get_affine_transform(c, s, r, self.image_size)
+        input = cv2.warpAffine(
+            data_numpy,
+            trans,
+            (int(self.image_size[0]), int(self.image_size[1])),
+            flags=cv2.INTER_LINEAR)
+
+
+        if self.transform:
+            input = self.transform(input)
+
+        for i in range(self.num_joints):
+            if joints_vis[i, 0] > 0.0:
+                joints[i, 0:2] = affine_transform(joints[i, 0:2], trans)
+
+        target, target_weight = self.generate_target(joints, joints_vis)
+
+        target = torch.from_numpy(target)
+        target_weight = torch.from_numpy(target_weight)
+
+        meta = {
+            'image': image_file,
+            'filename': filename,
+            'imgnum': imgnum,
+            'joints': joints,
+            'joints_vis': joints_vis,
+            'center': c,
+            'scale': s,
+            'rotation': r,
+            'score': score
+        }
+
+        return input, target, target_weight, meta
+
+
+    def generate_target(self, joints, joints_vis):
+        '''
+        :param joints:  [num_joints, 3]
+        :param joints_vis: [num_joints, 3]
+        :return: target, target_weight(1: visible, 0: invisible)
+        '''
+        target_weight = np.ones((self.num_joints, 1), dtype=np.float32)
+        target_weight[:, 0] = joints_vis[:, 0]
+
+
+        target = np.zeros((self.num_joints,
+                            self.heatmap_size[1],
+                            self.heatmap_size[0]),
+                            dtype=np.float32)
+
+        tmp_size = self.sigma * 3
+
+        for joint_id in range(self.num_joints):
+            feat_stride = self.image_size / self.heatmap_size
+            mu_x = int(joints[joint_id][0] / feat_stride[0] + 0.5)
+            mu_y = int(joints[joint_id][1] / feat_stride[1] + 0.5)
+            # Check that any part of the gaussian is in-bounds
+            ul = [int(mu_x - tmp_size), int(mu_y - tmp_size)]
+            br = [int(mu_x + tmp_size + 1), int(mu_y + tmp_size + 1)]
+            if ul[0] >= self.heatmap_size[0] or ul[1] >= self.heatmap_size[1] \
+                    or br[0] < 0 or br[1] < 0:
+                # If not, just return the image as is
+                target_weight[joint_id] = 0
+                continue
+
+            # # Generate gaussian
+            size = 2 * tmp_size + 1
+            x = np.arange(0, size, 1, np.float32)
+            y = x[:, np.newaxis]
+            x0 = y0 = size // 2
+            # The gaussian is not normalized, we want the center value to equal 1
+            g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * self.sigma ** 2))
+
+            # Usable gaussian range
+            g_x = max(0, -ul[0]), min(br[0], self.heatmap_size[0]) - ul[0]
+            g_y = max(0, -ul[1]), min(br[1], self.heatmap_size[1]) - ul[1]
+            # Image range
+            img_x = max(0, ul[0]), min(br[0], self.heatmap_size[0])
+            img_y = max(0, ul[1]), min(br[1], self.heatmap_size[1])
+
+            v = target_weight[joint_id]
+            if v > 0.5:
+                target[joint_id][img_y[0]:img_y[1], img_x[0]:img_x[1]] = \
+                    g[g_y[0]:g_y[1], g_x[0]:g_x[1]]
+
+
+        return target, target_weight
+    
+    def _xywh2cs(self, x, y, w, h):
+        center = np.zeros((2), dtype=np.float32)
+        center[0] = x + w * 0.5
+        center[1] = y + h * 0.5
+
+        if w > self.aspect_ratio * h:
+            h = w * 1.0 / self.aspect_ratio
+        elif w < self.aspect_ratio * h:
+            w = h * self.aspect_ratio
+            
+        scale = np.array(
+            [w * 1.0 , h * 1.0],
+            dtype=np.float32)
+        if center[0] != -1:
+            scale = scale * 1.25
+
+        return center, scale
+    
+    def _get_db(self):
+        # create train/val split
+        #anno = dlib_keypoints.load(os.path.join(self.root,self.image_set+'.xml'))
+        gt_db = []
+        coco = COCO(os.path.join(self.root,self.image_set+'.json'))
+        self.coco = coco
+        
+        cats = [cat['name'] for cat in self.coco.loadCats(self.coco.getCatIds())]
+        self.classes = ['__background__'] + cats
+        logger.info('=> classes: {}'.format(self.classes))
+        self.num_classes = len(self.classes)
+        self._class_to_ind = dict(zip(self.classes, range(self.num_classes)))
+        self._class_to_coco_ind = dict(zip(cats, self.coco.getCatIds()))
+        self._coco_ind_to_class_ind = dict(
+            [
+                (self._class_to_coco_ind[cls], self._class_to_ind[cls])
+                for cls in self.classes[1:]
+            ]
+        )
+        self._path_to_ind = dict()
+        
+        cats = coco.loadCats(coco.getCatIds())
+        num_joints_all = []
+        for cat in cats:
+            cat_name = cat['name']
+            catIds = coco.getCatIds(catNms=[cat_name])
+            imgIds = coco.getImgIds(catIds=catIds)
+            for imgid in imgIds:
+                image = coco.loadImgs(imgid)[0]
+                image_path = os.path.join(self.root,image['file_name'])
+                self._path_to_ind[image_path] = imgid
+                annIds = coco.getAnnIds(imgIds=imgid, catIds=catIds, iscrowd=None)
+                anns = coco.loadAnns(annIds)
+                for ann in anns:
+                    x,y,w,h = ann['bbox']
+                    c,s = self._xywh2cs(x,y,w,h)
+                    num_joints = ann['num_keypoints']
+                    num_joints_all.append(num_joints)
+                    joints_3d = np.zeros((num_joints, 3), dtype=np.float)
+                    joints_3d_vis = np.zeros((num_joints,  3), dtype=np.float)
+                    
+                    for ipt in range(num_joints):
+                        joints_3d[ipt, 0] = ann['keypoints'][ipt * 3 + 0]
+                        joints_3d[ipt, 1] = ann['keypoints'][ipt * 3 + 1]
+                        joints_3d[ipt, 2] = 0
+                        t_vis = ann['keypoints'][ipt * 3 + 2]
+                        if t_vis > 1:
+                            t_vis = 1
+                        joints_3d_vis[ipt, 0] = t_vis
+                        joints_3d_vis[ipt, 1] = t_vis
+                        joints_3d_vis[ipt, 2] = 0
+                    image_dir = 'images.zip@' if self.data_format == 'zip' else 'images'
+                    gt_db.append(
+                        {
+                            'image': image_path,
+                            'center': c,
+                            'scale': s,
+                            'joints_3d': joints_3d,
+                            'joints_3d_vis': joints_3d_vis,
+                            'filename': '',
+                            'imgnum': 0,
+                        }
+                    )
+        assert len(set(num_joints_all)) == 1, "num of keypoints should be the same"
+        self.num_joints = num_joints_all[0]
+        return gt_db
+
+    def evaluate(self, cfg, preds, output_dir, all_boxes, img_path,
+                 *args, **kwargs):
+
+        # person x (keypoints)
+        _kpts = []
+        for idx, kpt in enumerate(preds):
+            _kpts.append({
+                'keypoints': kpt,
+                'center': all_boxes[idx][0:2],
+                'scale': all_boxes[idx][2:4],
+                'area': all_boxes[idx][4],
+                'score': all_boxes[idx][5],
+                'image': self._path_to_ind[img_path[idx]]
+            })
+        # image x person x (keypoints)
+        kpts = defaultdict(list)
+        for kpt in _kpts:
+            kpts[kpt['image']].append(kpt)
+
+        # rescoring and oks nms
+        num_joints = self.num_joints
+        in_vis_thre = 0.00
+        oks_thre = 0.5
+        oks_nmsed_kpts = []
+        for img in kpts.keys():
+            img_kpts = kpts[img]
+            for n_p in img_kpts:
+                box_score = n_p['score']
+                kpt_score = 0
+                valid_num = 0
+                for n_jt in range(0, num_joints):
+                    t_s = n_p['keypoints'][n_jt][2]
+                    if t_s > in_vis_thre:
+                        kpt_score = kpt_score + t_s
+                        valid_num = valid_num + 1
+                if valid_num != 0:
+                    kpt_score = kpt_score / valid_num
+                # rescoring
+                n_p['score'] = kpt_score * box_score
+
+            if True:
+                keep = soft_oks_nms(
+                    [img_kpts[i] for i in range(len(img_kpts))],
+                    oks_thre,sigmas=copy.deepcopy(self.sigmas )
+                )
+            else:
+                keep = oks_nms(
+                    [img_kpts[i] for i in range(len(img_kpts))],
+                    oks_thre
+                )
+
+            if len(keep) == 0:
+                oks_nmsed_kpts.append(img_kpts)
+            else:
+                oks_nmsed_kpts.append([img_kpts[_keep] for _keep in keep])
+                
+        res_folder = os.path.join(output_dir, 'results')
+        if not os.path.exists(res_folder):
+            try:
+                os.makedirs(res_folder)
+            except Exception:
+                logger.error('Fail to make {}'.format(res_folder))
+        rank = 0 ##!!!!!!!!!
+        res_file = os.path.join(
+            res_folder, 'keypoints_{}_results_{}.json'.format(
+                self.image_set, rank)
+        )                
+        self._write_coco_keypoint_results(
+            oks_nmsed_kpts, res_file)
+        if 'test' not in self.image_set:
+            info_str = self._do_python_keypoint_eval(
+                res_file, res_folder)
+            name_value = OrderedDict(info_str)
+            return name_value, name_value['AP']
+        else:
+            return {'Null': 0}, 0            
+        return 
+    
+    def _write_coco_keypoint_results(self, keypoints, res_file):
+        data_pack = [
+            {
+                'cat_id': self._class_to_coco_ind[cls],
+                'cls_ind': cls_ind,
+                'cls': cls,
+                'ann_type': 'keypoints',
+                'keypoints': keypoints
+            }
+            for cls_ind, cls in enumerate(self.classes) if not cls == '__background__'
+        ]
+
+        results = self._coco_keypoint_results_one_category_kernel(data_pack[0])
+        logger.info('=> writing results json to %s' % res_file)
+        with open(res_file, 'w') as f:
+            json.dump(results, f, sort_keys=True, indent=4)
+        try:
+            json.load(open(res_file))
+        except Exception:
+            content = []
+            with open(res_file, 'r') as f:
+                for line in f:
+                    content.append(line)
+            content[-1] = ']'
+            with open(res_file, 'w') as f:
+                for c in content:
+                    f.write(c)
+
+    def _coco_keypoint_results_one_category_kernel(self, data_pack):
+        cat_id = data_pack['cat_id']
+        keypoints = data_pack['keypoints']
+        cat_results = []
+
+        for img_kpts in keypoints:
+            if len(img_kpts) == 0:
+                continue
+
+            _key_points = np.array([img_kpts[k]['keypoints']
+                                    for k in range(len(img_kpts))])
+            key_points = np.zeros(
+                (_key_points.shape[0], self.num_joints * 3), dtype=np.float
+            )
+
+            for ipt in range(self.num_joints):
+                key_points[:, ipt * 3 + 0] = _key_points[:, ipt, 0]
+                key_points[:, ipt * 3 + 1] = _key_points[:, ipt, 1]
+                key_points[:, ipt * 3 + 2] = _key_points[:, ipt, 2]  # keypoints score.
+
+            result = [
+                {
+                    'image_id': img_kpts[k]['image'],
+                    'category_id': cat_id,
+                    'keypoints': list(key_points[k]),
+                    'score': img_kpts[k]['score'],
+                    'center': list(img_kpts[k]['center']),
+                    'scale': list(img_kpts[k]['scale'])
+                }
+                for k in range(len(img_kpts))
+            ]
+            cat_results.extend(result)
+
+        return cat_results
+
+    def _do_python_keypoint_eval(self, res_file, res_folder):
+        coco_dt = self.coco.loadRes(res_file)
+        coco_eval = COCOeval(self.coco, coco_dt, 'keypoints')
+        coco_eval.params.useSegm = None
+        coco_eval.params.kpt_oks_sigmas = copy.deepcopy(self.sigmas )
+        coco_eval.evaluate()
+        coco_eval.accumulate()
+        coco_eval.summarize()
+
+        stats_names = ['AP', 'Ap .5', 'AP .75', 'AP (M)', 'AP (L)', 'AR', 'AR .5', 'AR .75', 'AR (M)', 'AR (L)']
+
+        info_str = []
+        for ind, name in enumerate(stats_names):
+            info_str.append((name, coco_eval.stats[ind]))
+
+        return info_str    
